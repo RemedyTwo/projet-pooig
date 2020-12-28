bd0ea37d01d7fd17e133335719fbedc579a1075a
romele <gaspardromele@gmail.com>
2020-12-28 15:47:03 +0100

    ajout joueur robot

--- src/Joueur.java
+++ src/Joueur.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class Joueur {
 //Ce code prend les commandes du joueur et les éxecute.
 
-	public int nbTours,nbVies,score;
-	boolean fini;
+	public int nbTours,score;
+	boolean fini,robot;
 	public Grille grille;
 	
-	public Joueur(int nbTours,Grille g){
+	public Joueur(int nbTours,Grille g,boolean robot){
 		this.nbTours=nbTours;
 		this.fini=false;
 		this.grille=g;
 		this.score=0;
-		this.nbVies=3;
+		this.robot=robot;
 	}
 	
 	public void finDuJeu(){
@@ -30,18 +30,63 @@ public class Joueur {
 		while(!fini){
 			Scanner scanner = new Scanner(System.in); 
 			grille.affichage();
-			System.out.print("colonne :");
-			int x=scanner.nextInt();
-			System.out.println("\n");
-			System.out.print("ligne :");
-			int y=scanner.nextInt();
+			int x=0;
+			int y=0;
+			if(robot){
+			//Si le joueur est un robot, il prend la case avec le plus grand nombre de cubes qu'il peut supprimer.
+				int max=0;
+				for(int i=0;i<grille.length;i++){
+					for(int j=0;j<grille[i].length;j++){
+						if(max<grille.adjacences(i,j)){
+							max=grille.adjacences(i,j);
+						}
+					}
+				}
+				while(grille.adjacences(x,y)<max && x<grille.length){
+					while(grille.adjacences(x,y)<max && y<grille[x].length){
+						y++;
+					}
+					x++;
+				}
+			}else{	
+				System.out.print("colonne :");
+				x=scanner.nextInt();
+				System.out.println("\n");
+				System.out.print("ligne :");
+				y=scanner.nextInt();
+			}
 			if(grille.peutSupprimer(x,y)){
+				int adj=grille.adjacences(x,y);
+				score+=adj*100;
+				if(adj>3 && adj<9){
+					score+=250;
+				}else if(adj>9){
+					score+=500;
+				}
 				grille.supprimeCube(x,y);
 				grille.gravite();
+				nbTours--;
 			}else{
 				System.out.println("Tour invalide");
 			}
 			finDuJeu();
+			tour();
+		}
+	}
+	
+	public void lanceJeu(){
+		if(!robot){
+			Scanner scanner = new Scanner(System.in); 
+			System.out.println("Voulez-vous jouer? (Y/N)");
+			String s=scanner.nextLine();
+			if(s.equals"Y"){
+				tour();
+			}if(s.equals "N"){
+				System.out.println("D'accord, au revoir.");
+			}else{
+				System.out.println("Commande invalide");
+			}
+		}else{
 			tour();
 		}
 	}
